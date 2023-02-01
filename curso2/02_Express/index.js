c5d8353a790b489505d6976f5b4d7a245652e3c6
Rudge Santos <rudges86@gmail.com>
2023-02-01 18:23:45 -0300

    enviado sem o node-modules

--- curso2/02_Express/index.js
+++ curso2/02_Express/index.js
@@ -2,6 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req,res)=>{
+    res.send('<h1>Recebido!!</h1>')
+    
+});
+
+app.get("/blog",(req,res)=>{
+    res.send("Bem-vindo ao meu blog!!")
+});
+
+app.get("/canal/youtube",(req,res)=>{
+    res.send("<h1>Se inscreva no meu canal !!!</h1>")
+});
+
+
+
 app.listen(4000,(erro)=>{
         if(erro){
             console.log(erro)
