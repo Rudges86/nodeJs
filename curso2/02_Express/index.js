0c61bf9cf6cb4a56a3480a8639d0af9e6d5b6a91
Rudge Santos <rudges86@gmail.com>
2023-02-02 09:08:19 -0300

    adicionado mais rotas gets, com parâmetros

--- curso2/02_Express/index.js
+++ curso2/02_Express/index.js
@@ -2,25 +2,44 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send('<h1>Recebido!!</h1>')
-    
+
 });
 
-app.get("/blog",(req,res)=>{
+app.get("/blog", (req, res) => {
     res.send("Bem-vindo ao meu blog!!")
 });
 
-app.get("/canal/youtube",(req,res)=>{
+app.get("/canal/youtube", (req, res) => {
     res.send("<h1>Se inscreva no meu canal !!!</h1>")
 });
 
+app.get("/parm/:nome", (req, res) => {
+    let nome = req.params.nome;
+    res.send(`<h1>Oi! ${nome} </h1>`)
+})
+
+app.get("/parm/:nome/:empresa", (req, res) => {
+    let nome = req.params.nome;
+    let empresa = req.params.empresa;
+    res.send(`<h1>Oi! ${nome} da empresa: ${empresa} </h1>`)
+})
+
+app.get("/bloc/:fulano?",(req,res)=>{
+    let teste = res.params.fulano;
+    if(teste){
+        res.send(`Opcional ${req.params.fulano}`)
+    }else{
+        res.send("Faltou o parâmetro")
+    }
+})
 
 
-app.listen(4000,(erro)=>{
-        if(erro){
-            console.log(erro)
-        }else{
-            console.log("Servidor iniciado! rodando na porta 4000")
-        }
+app.listen(4000, (erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log("Servidor iniciado! rodando na porta 4000")
+    }
 });
